4d8d2e3f6ef4f9eae41702f4013fbc4bf2223dde
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-05 16:09:39 +0100

    Removed useless functions

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,24 +2,6 @@ from graph import Graph
 from util import Stack
 
 
-def find_ancestors(ancestors, starting_node):
-    # create an empty list ancestors
-    direct_ancestors = []
-    # loop through the input list of tuples
-    for parent, child in ancestors:
-        # see if the child is starting_node
-        if child == starting_node:
-            # add the parent to direct_ancestors
-            direct_ancestors.append(parent)
-
-    # if direct_ancestors is empty return -1
-    if not len(direct_ancestors):
-        return -1
-    # otherwise, return direct_ancestors
-    else:
-        return direct_ancestors
-
-
 def earliest_ancestor(ancestors, starting_node):
     # FIRST REPRESENT THE INPUT ANCESTORS AS A GRAPH
     graph = Graph()
@@ -35,8 +17,6 @@ def earliest_ancestor(ancestors, starting_node):
     for parent, child in ancestors:
         # connect the parent to the child
         graph.add_edge(child, parent)
-
-    print(graph.vertices)
 
     # return -1 if input has no value
     if not graph.vertices[starting_node]:
@@ -65,12 +45,3 @@ def earliest_ancestor(ancestors, starting_node):
             s.push(v)
 
     return earliest_anc
-
-
-if __name__ == '__main__':
-    test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6),
-                      (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-
-    print(find_ancestors(test_ancestors, 3))  # output should be [1, 2]
-    print(find_ancestors(test_ancestors, 1))  # should output [1]
-    print(find_ancestors(test_ancestors, 11))  # should output [-1]
