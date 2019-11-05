002e4a4a0e5c97b9013bbadabea155b2bb58ee31
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-05 14:04:57 +0100

    Implemented the pseudocode

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,10 +1,19 @@
 def find_ancestors(ancestors, starting_node):
     # create an empty list ancestors
-    # loop through the input list of tuples and see if the child is starting_node
-        # add the parent to ancestors
+    ancestors = []
+    # loop through the input list of tuples
+    for parent, child in ancestors:
+        # see if the child is starting_node
+        if child == starting_node:
+            # add the parent to ancestors
+            ancestors.append(parent)
+
     # if ancestors is empty return -1
+    if not len(ancestors):
+        return -1
     # otherwise, return ancestors
-    pass
+    else:
+        return ancestors
 
 
 def earliest_ancestor(ancestors, starting_node):
