7e818880c7f6890db111f1a3f2f9172e210ef393
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-05 14:02:02 +0100

    pseudocode for the find ancestor function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,2 +1,11 @@
+def find_ancestors(ancestors, starting_node):
+    # create an empty list ancestors
+    # loop through the input list of tuples and see if the child is starting_node
+        # add the parent to ancestors
+    # if ancestors is empty return -1
+    # otherwise, return ancestors
+    pass
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
